550279e7903f7b01ad0742ea1c57ae9f208ea657
YakultLover <rianbowgift@gmail.com>
2023-08-20 03:17:58 +0900

    생각보다 까다로운 파이썬배열 사용법

--- BAEKJOON/Base/BJ_2738.py
+++ BAEKJOON/Base/BJ_2738.py
@@ -1,3 +1,17 @@
 n,m = map(int,input().split())
 
+a,b = [],[]
+
+
 for i in range(n):
+    rows = list(map(int,input().split()))
+    a.append(rows)
+
+for i in range(n):
+    rows = list(map(int,input().split()))
+    b.append(rows)
+
+for i in range(n):
+    for j in range(m):
+        print(a[i][j] + b[i][j],end=' ')
+    print()
