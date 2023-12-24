e5effeb7cb43286941ad8ab48710ccc1ed9d2e3b
rianb <rianbowgift@gmail.com>
2023-12-25 00:18:11 +0900

    b = list(map(int,input().split())) 사용법 숙지

--- BAEKJOON/Base/BJ_28938.py
+++ BAEKJOON/Base/BJ_28938.py
@@ -1,15 +1,15 @@
 a = int(input())
-sum=0
+b = list(map(int,input().split()))
+c = 0
 for i in range(a):
-    b = int(input())
-    if b==1:
-        sum+=1
-    elif b==-1:
-        sum-=1
+    if b[i]==1:
+        c+=1
+    elif b[i]==-1:
+        c-=1
 
-if sum==0:
+if c==0:
     print("Stay")
-elif sum>0:
+elif c>0:
     print("Right")
 else:
     print("Left")
