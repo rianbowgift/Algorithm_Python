7c85988b8de0fc68edb552e3388f1d13977ed4b6
rianb <rianbowgift@gmail.com>
2023-12-26 20:36:31 +0900

    b = list(map(int,input().split())) 사용법 숙지2

--- BAEKJOON/Base/BJ_29163.py
+++ BAEKJOON/Base/BJ_29163.py
@@ -1,1 +1,14 @@
-pass
+a = int(input())
+b = list(map(int,input().split()))
+d=0
+e=0
+for i in range(a):
+   if b[i]%2==0:
+       d+=1
+   else:
+       e+=1
+
+if d>e:
+    print("Happy")
+else:
+    print("Sad")
