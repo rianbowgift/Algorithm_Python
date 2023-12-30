75e0221d5adba5ad164c71f41f86429d6a46fd9f
rianb <rianbowgift@gmail.com>
2023-12-31 05:37:09 +0900

    FreeBASIC

---
+++ BAEKJOON/Base/BJ_2377.py
@@ -0,0 +1,13 @@
+# PRINT " _________"
+# PRINT " \_     _/"
+# PRINT "   \   /"
+# PRINT "    | |"
+# PRINT "   /   \"
+# PRINT "  /     \"
+# PRINT " |       |"
+# PRINT "/---------\"
+# PRINT "| \-/ \-/ |"
+# PRINT "\---------/"
+# PRINT " \_______/"
+
+#FreeBASIC 전용으로 파이썬으로는 해결불가
