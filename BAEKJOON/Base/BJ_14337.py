f913c6866d1c1a6097bba786b525a0bd49ad1051
rianb <rianbowgift@gmail.com>
2024-01-01 01:13:56 +0900

    Visual Basic/브5 end

---
+++ BAEKJOON/Base/BJ_14337.py
@@ -0,0 +1,14 @@
+# Module Helicopter
+#     Sub Main()
+#         Console.WriteLine("      ===================")
+#         Console.WriteLine("          ____||___")
+#         Console.WriteLine("\ /      /       []\")
+#         Console.WriteLine(" X=======           \__")
+#         Console.WriteLine("/ \      \____________|")
+#         Console.WriteLine("            ||  ||")
+#         Console.WriteLine("         \-----------/")
+#     End Sub
+# End Module
+
+
+#Visual Basic 전용으로 파이썬으로는 해결불가
