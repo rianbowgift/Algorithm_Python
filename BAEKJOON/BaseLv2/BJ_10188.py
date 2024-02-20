febde6f9a9ed5737dec16d6d9d2588101b309788
rianb <rianbowgift@gmail.com>
2024-02-20 19:57:57 +0900

    간단별찍기

---
+++ BAEKJOON/BaseLv2/BJ_10188.py
@@ -0,0 +1,8 @@
+a = int(input())
+for i in range(0,a):
+    b,c = map(int,input().split())
+    for n in range(0,c):
+        for m in range(0,b):
+            print("X",end='')
+        print("")
+    print("")
