ec9b44045bf579c7f7a209b920796ac50293e100
rianb <rianbowgift@gmail.com>
2024-03-07 22:31:43 +0900

    문제만어려움

---
+++ BAEKJOON/BaseLv2/BJ_13118.py
@@ -0,0 +1,12 @@
+a= list(map(int,input().split()))
+e,f,g = map(int,input().split())
+
+x=0
+for i in a:
+    if i==e:
+        print(x+1)
+        break
+    else:
+        x=x+1
+if x==4:
+    print(0)
