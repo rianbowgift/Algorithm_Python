f995d87c85b4683dafaaf6a9694ddc08b7406cba
rianb <rianbowgift@gmail.com>
2024-04-04 06:20:11 +0900

    간단 형변환

---
+++ BAEKJOON/BaseLv2/BJ_15873.py
@@ -0,0 +1,10 @@
+a = input()
+if len(a)==2:
+    print(int(a[0])+int(a[1]))
+elif len(a)==3:
+    if a[1]=='0':
+        print(10+int(a[2]))
+    else:
+        print(int(a[0])+10)
+else:
+    print(20)
