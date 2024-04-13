6e297497f67b4b9a31b6705587e07e57616bcea7
rianb <rianbowgift@gmail.com>
2024-04-13 16:27:44 +0900

    간단분기

---
+++ BAEKJOON/BaseLv2/BJ_17009.py
@@ -0,0 +1,13 @@
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+e = int(input())
+f = int(input())
+
+if (a*3)+(b*2)+c>(d*3)+(e*2)+f:
+    print("A")
+elif (a*3)+(b*2)+c<(d*3)+(e*2)+f:
+    print("B")
+else:
+    print("T")
