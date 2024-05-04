3e5d414917accdccb949b8185424c888d8cd877c
rianb <rianbowgift@gmail.com>
2024-05-05 04:44:28 +0900

    계산조건분기 3.5

---
+++ BAEKJOON/BaseLv2/BJ_20233.py
@@ -0,0 +1,20 @@
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+e = int(input())
+
+f = e-30
+g = e-45
+
+if f>0:
+    print (a + (f*b*21),end= ' ')
+else:
+    print(a,end= ' ')
+
+if g>0:
+    print (c + (g*d*21),end= '')
+else:
+    print(c,end= '')
+
+
