7de6b8b595e8953e6657653d24051c3e4388d800
rianb <rianbowgift@gmail.com>
2024-08-12 05:13:36 +0900

    배열활용 1.9

---
+++ BAEKJOON/BaseLv2/BJ_28290.py
@@ -0,0 +1,7 @@
+a = {'fdsajkl;' : 'in-out', 'asdf;lkj' : 'out-in', 'asdfjkl;' : 'stairs' , ';lkjfdsa' : 'reverse' , 'jkl;fdsa' : 'in-out', ';lkjasdf': 'out-in'}
+b = input()
+
+try:
+    print(a[b])
+except:
+    print("molu")
